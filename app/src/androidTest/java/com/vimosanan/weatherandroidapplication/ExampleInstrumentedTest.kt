33499356e4b012b402fa6df86d46acad387a630e
vimosanana <vimosanana@ellipsis.lk>
2020-02-20 16:07:48 +0800

    implement ui test

--- app/src/androidTest/java/com/vimosanan/weatherandroidapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/vimosanan/weatherandroidapplication/ExampleInstrumentedTest.kt
@@ -1,12 +1,21 @@
 package com.vimosanan.weatherandroidapplication
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.vimosanan.weatherandroidapplication.ui.main.WeatherActivity
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +24,29 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @Rule
+    @JvmField
+    val rule: ActivityTestRule<WeatherActivity> = ActivityTestRule(WeatherActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.vimosanan.weatherandroidapplication", appContext.packageName)
+    }
+
+    @Test
+    fun user_can_enter_city(){
+        onView(withId(R.id.edtTxtSearch)).perform(typeText("Colombo"))
+    }
+
+    @Test
+    fun user_can_search(){
+        onView(withId(R.id.edtTxtSearch)).perform(typeText("Colombo"))
+
+        onView(withId(R.id.btnSearch)).perform(click())
+
+        onView(withId(R.id.txtInfo)).check(matches(withText("loading data from network")))
     }
 }
